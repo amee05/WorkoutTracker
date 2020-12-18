e0f9e80c1585c94d0b46b8166b5795c01299d4f3
Amee Parmar <amee05@yahoo.com>
2020-12-18 14:38:24 -0800

    code

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -1,12 +1,18 @@
 const router = require('express').Router()
 const { join } = require('path')
 
+
+
 router.get('/exercise', (req, res) => {
   res.sendFile(join(__dirname, '../public/exercise.html'))
 })
 
 router.get('/stats', (req, res) => {
   res.sendFile(join(__dirname, '../public/stats.html'))
+})
+
+router.get('/index', (req, res) => {
+  res.sendFile(join(__dirname, '../public/index.html'))
 })
 
 module.exports = router
